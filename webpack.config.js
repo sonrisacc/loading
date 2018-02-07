dcb0ccea425282a5f7d2360158627bf82bb711fd
Sonrisa <sonrisa722611@gmail.com>
2018-02-06 18:01:32 -0800

    Update inital folder structure setup

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  // augmenting abilities of webpack
+  // will send down module name for debugging purposes
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
